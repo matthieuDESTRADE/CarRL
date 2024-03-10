9b88a35b57abca265201c586a032b04a1f68c1ed
pe712 <pe.baviere@gmail.com>
2024-03-10 13:43:09 +0100

    separate policy file

--- deep_Q_newtork.py
+++ deep_Q_newtork.py
@@ -8,93 +8,6 @@ import itertools
 from torchrl.data import ReplayBuffer
 
 
-class QNetwork(nn.Module):
-    def __init__(self, action_dim):
-        super().__init__()
-
-        # Define the layers of the CNN
-        self.conv1 = nn.Conv2d(
-            in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
-        self.relu1 = nn.LeakyReLU(0.1)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(
-            in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
-        self.relu2 = nn.LeakyReLU(0.1)
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv3 = nn.Conv2d(
-            in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.relu3 = nn.LeakyReLU(0.1)
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(64*5*5, 32)
-        self.relu3 = nn.LeakyReLU(0.1)
-        self.fc2 = nn.Linear(32, action_dim)
-
-    def forward(self, x):
-        # Define the forward pass of the CNN
-        x = self.conv1(x)
-        x = self.relu1(x)
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = self.relu2(x)
-        x = self.pool2(x)
-        x = self.conv3(x)
-        x = self.relu3(x)
-        x = self.pool3(x)
-        x = x.reshape(x.size(0), -1)
-        x = self.fc1(x)
-        x = self.relu3(x)
-        x = self.fc2(x)
-        return x
-
-
-class OtherQNetwork(nn.Module):
-    """
-    Expects a 3x40x40 input and returns a 6x1 output.
-
-    """
-
-    def __init__(self, action_dim):
-        super().__init__()
-        # Layer 1: Convolutional Layer, Pooling, ReLU
-        self.conv1 = nn.Conv2d(
-            in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.relu1 = nn.ReLU()
-
-        # Layer 2: Convolutional Layer, Pooling, ReLU
-        self.conv2 = nn.Conv2d(
-            in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1)
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.relu2 = nn.ReLU()
-
-        # Layer 3: Convolutional Layer, ReLU
-        self.conv3 = nn.Conv2d(
-            in_channels=32, out_channels=32, kernel_size=3, stride=3, padding=1)
-        self.relu3 = nn.ReLU()
-
-        # Layer 4: Fully Connected Layer, ReLU
-        self.fc1 = nn.Linear(32 * 4 * 4, 256)
-        self.relu4 = nn.ReLU()
-
-        # layer 5: LSTM
-        self.lstm = nn.LSTM(256, action_dim, batch_first=True)
-
-        self.model = nn.Sequential(
-            self.conv1, self.pool1, self.relu1,
-            self.conv2, self.pool2, self.relu2,
-            self.conv3, self.relu3,
-            nn.Flatten(),
-            self.fc1, self.relu4,
-            self.lstm,
-        )
-
-    def forward(self, x):
-        # Define the forward pass of the CNN
-        output, (hidden_state, cell_state) = self.model(x)
-        probas = nn.functional.softmax(output, dim=1)
-        return probas
-
-
 class EpsilonGreedy:
     def __init__(self,
                  epsilon_start: float,
@@ -174,7 +87,7 @@ class DQNAgent():
               lr_scheduler: torch.optim.lr_scheduler,
               ):
         iteration = 0
-        self.train_index +=1
+        self.train_index += 1
 
         for episode_index in tqdm(range(1, num_episodes)):
             state = self.env.reset()
@@ -194,7 +107,8 @@ class DQNAgent():
 
                 next_state, reward, done, _ = self.env.step(action)
 
-                self.replay_buffer.add((state, action, reward, next_state, done))
+                self.replay_buffer.add(
+                    (state, action, reward, next_state, done))
 
                 episode_reward += reward
                 # Update the q_network weights with a batch of experiences from the buffer
@@ -204,7 +118,8 @@ class DQNAgent():
                         batch_size)
 
                     # Convert to PyTorch tensors
-                    batch_states_tensor = CarEnv.obs2tensor(batch_states, self.device)
+                    batch_states_tensor = CarEnv.obs2tensor(
+                        batch_states, self.device)
                     batch_actions_tensor = torch.tensor(
                         batch_actions, dtype=torch.long, device=self.device)
                     batch_rewards_tensor = torch.tensor(
---
+++ policy.py
@@ -0,0 +1,89 @@
+"""Since the input space is large we will use CNN"""
+from torch import nn
+
+
+class QNetwork(nn.Module):
+    def __init__(self, action_dim):
+        super().__init__()
+
+        # Define the layers of the CNN
+        self.conv1 = nn.Conv2d(
+            in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.LeakyReLU(0.1)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(
+            in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.LeakyReLU(0.1)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv3 = nn.Conv2d(
+            in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.relu3 = nn.LeakyReLU(0.1)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(64*5*5, 32)
+        self.relu3 = nn.LeakyReLU(0.1)
+        self.fc2 = nn.Linear(32, action_dim)
+
+    def forward(self, x):
+        # Define the forward pass of the CNN
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.pool2(x)
+        x = self.conv3(x)
+        x = self.relu3(x)
+        x = self.pool3(x)
+        x = x.reshape(x.size(0), -1)
+        x = self.fc1(x)
+        x = self.relu3(x)
+        x = self.fc2(x)
+        return x
+
+
+class OtherQNetwork(nn.Module):
+    """
+    Expects a 3x40x40 input and returns a 6x1 output.
+
+    """
+
+    def __init__(self, action_dim):
+        super().__init__()
+        # Layer 1: Convolutional Layer, Pooling, ReLU
+        self.conv1 = nn.Conv2d(
+            in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.relu1 = nn.ReLU()
+
+        # Layer 2: Convolutional Layer, Pooling, ReLU
+        self.conv2 = nn.Conv2d(
+            in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.relu2 = nn.ReLU()
+
+        # Layer 3: Convolutional Layer, ReLU
+        self.conv3 = nn.Conv2d(
+            in_channels=32, out_channels=32, kernel_size=3, stride=3, padding=1)
+        self.relu3 = nn.ReLU()
+
+        # Layer 4: Fully Connected Layer, ReLU
+        self.fc1 = nn.Linear(32 * 4 * 4, 256)
+        self.relu4 = nn.ReLU()
+
+        # layer 5: LSTM
+        self.lstm = nn.LSTM(256, action_dim, batch_first=True)
+
+        self.model = nn.Sequential(
+            self.conv1, self.pool1, self.relu1,
+            self.conv2, self.pool2, self.relu2,
+            self.conv3, self.relu3,
+            nn.Flatten(),
+            self.fc1, self.relu4,
+            self.lstm,
+        )
+
+    def forward(self, x):
+        # Define the forward pass of the CNN
+        output, (hidden_state, cell_state) = self.model(x)
+        probas = nn.functional.softmax(output, dim=1)
+        return probas
